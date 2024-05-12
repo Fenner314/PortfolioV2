02d63e76df15f7e30eb8534a8aa2c8bef79e39cd
Jacob Fenner <jacobgfenner@gmail.com>
2024-05-12 11:56:43 -0500

    Set up project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import './Header.scss'
+
+interface HeaderProps {}
+
+const Header: FC<HeaderProps> = () => (
+	<div className='Header'>Header Component</div>
+)
+
+export default Header
